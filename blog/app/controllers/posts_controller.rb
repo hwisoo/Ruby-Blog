b68bb9310280d3c6c058bee3e25c4a8555d1cc6e
James Cho <hwisoocho@gmail.com>
2018-09-10 14:07:27 -0700

    edit and delete

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -4,17 +4,41 @@ class PostsController < ApplicationController
     end
 
     def new
-    end
-
-    def create
-        @post = Post.new(post_params)
-        @post.save
-
-        redirect_to @post
+        @post = Post.new
     end
 
     def show
         @post = Post.find(params[:id])
+    end
+
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to @post
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        
+        if @post.update(params[:post].permit(:title, :body))
+            redirect_to @post
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+
+        redirect_to root_path
     end
 
     private
